8bae909b0d29a7e5cf10ecdead0d245e6b323212
shubham <shubhamndeshmukh02@gmail.com>
2026-05-24 13:23:00 +0530

    rds

--- EKS-Terraform/rds.tf
+++ EKS-Terraform/rds.tf
@@ -1,31 +1,50 @@
-resource "aws_db_instance" "rds" {
-  allocated_storage      = 20
-  identifier = "microservices-rds"
-  db_subnet_group_name   = aws_db_subnet_group.sub-grp.id
-  engine                 = "mysql"
-  engine_version         = "8.4.8"
-  instance_class         = "db.t3.micro"
-  multi_az               = true
-  db_name                = "mydb"
-  username               = "admin"
-  password               = "Cloud123"
-  skip_final_snapshot    = true
-  vpc_security_group_ids = [aws_security_group.allow_all.id]
-  depends_on = [ aws_db_subnet_group.sub-grp ]
-  publicly_accessible = true
-  backup_retention_period = 7
+resource "aws_db_subnet_group" "sd_sub_grp" {
 
-  
+  name = "sd-db-subnet-group"
+
+  subnet_ids = [
+    aws_subnet.sd_private1.id,
+    aws_subnet.sd_private2.id
+  ]
+
   tags = {
-    DB_identifier = "book-rds"
+    Name = "sd-db-subnet-group"
   }
 }
 
-resource "aws_db_subnet_group" "sub-grp" {
-  name       = "main"
-  subnet_ids = [aws_subnet.private1.id, aws_subnet.private2.id]
+resource "aws_db_instance" "sd_rds" {
+
+  allocated_storage = 20
+
+  identifier = "sd-microservices-rds"
+
+  db_subnet_group_name = aws_db_subnet_group.sd_sub_grp.name
+
+  engine         = "mysql"
+  engine_version = "8.4.8"
+
+  instance_class = "db.t3.micro"
+
+  multi_az = true
+
+  db_name  = "mydb"
+  username = "admin"
+  password = "Cloud123"
+
+  skip_final_snapshot = true
+
+  vpc_security_group_ids = [
+    aws_security_group.sd_allow_all.id
+  ]
+
+  publicly_accessible   = true
+  backup_retention_period = 7
+
+  depends_on = [
+    aws_db_subnet_group.sd_sub_grp
+  ]
 
   tags = {
-    Name = "My DB subnet group"
+    Name = "sd-book-rds"
   }
 }
